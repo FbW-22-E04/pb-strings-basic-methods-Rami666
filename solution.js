e47fd464e7aac7c1990a3c16cf36418614080133
Rami B <ramibasheer9@gmail.com>
2022-07-04 13:33:31 +0200

    submitting answers

--- solution.js
+++ solution.js
@@ -1,1 +1,66 @@
+const parkWalkStr = 'I can walk in the park all day!'
+console.log(parkWalkStr.indexOf('park'));
+console.log(parkWalkStr.substring(18, 22));
+
+console.log('---------------------');
+
+let helloStr = 'Hello Earthing'
+let upperHelloStr = helloStr.toLocaleUpperCase();
+console.log(upperHelloStr);
+
+console.log('---------------------');
+
+const earthLingsStr = 'Hello Earthling';
+const lowerEarthlingsStr = earthLingsStr.toLocaleLowerCase();
+console.log(lowerEarthlingsStr);
+
+console.log('---------------------');
+
+const jsStr = 'JavaScript';
+console.log(jsStr.substring(3, 6));
+
+console.log('---------------------');
+
+const niceShoesStr = 'nice shoes'
+console.log(niceShoesStr.includes('l'));
+console.log(niceShoesStr.includes('n'));
+
+console.log('---------------------');
+
+const oldStr = 'j3for'
+const oldStrResult = 'J' + oldStr + 'J'
+console.log(oldStrResult);
+
+console.log('---------------------');
+
+const scriptcherStr = '7sako'
+const lastThreeChars = scriptcherStr.slice(-3);
+console.log(lastThreeChars + scriptcherStr + lastThreeChars);
+
+console.log('---------------------');
+
+const boogieNightsStr = 'Rami';
+const result = boogieNightsStr.replace('i', 'R');
+const lastResult = result.replace('i', 'R')
+const result2 = result.replace('R', 'i')
+console.log(result2);
+
+console.log('---------------------');
+
+const firstName = 'Maria',
+ city = 'Berlin',
+ job = 'Web Deb';
+ console.log('my name is', firstName, 'I live in', city, 'and I am a', job, '.');
+
+ console.log('---------------------');
+
+const foxStr = 'the quick brown fox';
+const foxStr2 = foxStr.toUpperCase();
+console.log(foxStr2);
+
+console.log('---------------------');
+
+
+
+
 
